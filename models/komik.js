c3a522304de8ed3f65df15af7a48f401c8327d03
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-25 14:32:47 +0700

    Menyimpan data komik beserta informasi gambar (BLOB)

--- models/komik.js
+++ models/komik.js
@@ -1,27 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
+
+  // ================================
+  //  Model: Komik
+  //  Menyimpan data komik beserta informasi gambar (BLOB)
+  // ================================
   const Komik = sequelize.define('Komik', {
+
+    // Primary key otomatis bertambah
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
+
+    // Judul komik (wajib diisi)
     judul: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
+    // Nama penulis komik (wajib diisi)
     author: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
+    // Deskripsi komik (wajib diisi, tipe TEXT karena bisa panjang)
     deskripsi: {
       type: DataTypes.TEXT,
       allowNull: false
     },
 
+    // -----------------------------------------
+    // Penyimpanan gambar:
+    // imageType : tipe file (jpeg/png/webp)
+    // imageName : nama file aslinya
+    // imageData : binary data (disimpan dalam BLOB)
+    // -----------------------------------------
     imageType: DataTypes.STRING,
     imageName: DataTypes.STRING,
     imageData: DataTypes.BLOB('long'),
+
   }, {
+    // Nama tabel di database
     tableName: 'komik',
   });
 
